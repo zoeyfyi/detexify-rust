536e275242fcb5d8f1750478c5c41bc05ebcda34
Ben Sheffield <me@mrbenshef.co.uk>
2020-09-02 21:58:37 +0100

    test(lib): stroke sample

--- detexify/src/stroke_sample.rs
+++ detexify/src/stroke_sample.rs
@@ -15,6 +15,7 @@ pub struct StrokeSample {
 
 impl StrokeSample {
     pub fn new(mut strokes: Vec<Stroke>) -> Self {
+        strokes.truncate(10);
         for stroke in strokes.iter_mut() {
             stroke.dedup();
             stroke.smooth();
@@ -24,22 +25,7 @@ impl StrokeSample {
             stroke.dominant(2.0 * PI * 15.0 / 360.0)
         }
 
-        strokes.truncate(10);
-
         StrokeSample { strokes }
-
-        // StrokeSample {
-        //     strokes: strokes
-        //         .into_iter()
-        //         .map(unduplicate)
-        //         .map(smooth)
-        //         .map(|s| aspect_refit(Rect::new(ZERO_POINT, ONE_POINT), s))
-        //         .map(|s| redistribute(10, s))
-        //         .map(unduplicate)
-        //         .map(|s| dominant(2.0 * PI * 15.0 / 360.0, s))
-        //         .take(10)
-        //         .collect(),
-        // }
     }
 }
 
@@ -50,5 +36,71 @@ impl Sample<StrokeSample> for StrokeSample {
             Stroke::concat(a.strokes),
             Stroke::concat(b.strokes),
         )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::{point::Point, Stroke, StrokeSample};
+
+    #[test]
+    fn test_sample() {
+        let points = vec![
+            Point { x: 166.0, y: 80.0 },
+            Point { x: 156.0, y: 104.0 },
+            Point { x: 82.0, y: 182.0 },
+            Point { x: 48.0, y: 194.0 },
+            Point { x: 28.0, y: 127.0 },
+            Point { x: 39.0, y: 115.0 },
+            Point { x: 59.0, y: 106.0 },
+            Point { x: 120.0, y: 106.0 },
+            Point { x: 135.0, y: 115.0 },
+            Point { x: 149.0, y: 129.0 },
+            Point { x: 160.0, y: 145.0 },
+            Point { x: 207.0, y: 200.0 },
+        ];
+
+        let expected_points = vec![
+            Point {
+                x: 0.7569169960474308,
+                y: 0.1442687747035573,
+            },
+            Point {
+                x: 0.40477099163285507,
+                y: 0.5540160467382995,
+            },
+            Point {
+                x: 0.1626658042136413,
+                y: 0.6500724952223573,
+            },
+            Point {
+                x: 4.308955299830499e-3,
+                y: 0.4971489109610397,
+            },
+            Point {
+                x: 0.14894547060976254,
+                y: 0.3286712800588731,
+            },
+            Point {
+                x: 0.41775887969016,
+                y: 0.32246108567927273,
+            },
+            Point {
+                x: 0.651671871693856,
+                y: 0.44047560390735446,
+            },
+            Point {
+                x: 0.9999999999999999,
+                y: 0.8557312252964425,
+            },
+        ];
+
+        let sample = StrokeSample::new(vec![Stroke::new(points)]);
+        assert_eq!(sample.strokes.len(), 1);
+
+        for (i, &point) in sample.strokes[0].clone().points().iter().enumerate() {
+            assert!(Point::approx_eq(point, expected_points[i]));
+        }
     }
 }
