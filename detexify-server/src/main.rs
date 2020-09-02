0ae4960bdaf184d7acc00cae15462738da920a7a
Ben Sheffield <me@mrbenshef.co.uk>
2020-09-02 22:14:26 +0100

    chore(server): cleanup

--- detexify-server/src/main.rs
+++ detexify-server/src/main.rs
@@ -7,42 +7,30 @@ extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
 
-use detexify::Classifier;
+use detexify::{Classifier, StrokeSample};
 use rocket::State;
 use rocket_contrib::json::{Json, JsonValue};
 
 #[get("/")]
 fn get_root() -> JsonValue {
     json!({
-        "server": "Nöt Betty :(",
-        "version": "0.0.2",
+        "server": "rust detexify server",
+        "version": "0.1.0",
     })
 }
-
-// #[derive(Debug, Serialize, Deserialize)]
-// struct Point {
-//     t: i64,
-//     x: f64,
-//     y: f64,
-// }
-
-// type Stroke = Vec<Point>;
 
 #[post("/classify", format = "json", data = "<stroke_sample>")]
 fn post_classify(
     stroke_sample: Json<Vec<detexify::Stroke>>,
     classifier: State<Classifier>,
 ) -> Json<Vec<detexify::Score>> {
-
-    let results = classifier.classify(detexify::StrokeSample::new(stroke_sample.0));
+    let results = classifier.classify(StrokeSample::new(stroke_sample.0));
     Json(results)
 }
 
 fn main() {
     let classifier = Classifier::default();
 
-    // let snapshot_json: HashMap<String, Vec<Strokes>> = serde_json::from_reader(BufReader::new(file))?;
-    // println!("{}", snapshot_json);
     rocket::ignite()
         .mount("/", routes![get_root, post_classify])
         .manage(classifier)
