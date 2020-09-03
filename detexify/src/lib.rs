73bf8f1ccf4c30524315fbbe6c977df0cbacbd13
Ben Sheffield <me@mrbenshef.co.uk>
2020-09-03 14:05:29 +0100

    fix: export Point from root

--- detexify/src/lib.rs
+++ detexify/src/lib.rs
@@ -3,17 +3,11 @@
 pub mod point;
 pub mod rect;
 pub mod stroke;
-pub use self::stroke::*;
-pub use self::classifier::*;
 pub mod classifier;
 pub mod dtw;
 pub mod stroke_sample;
-pub use self::stroke_sample::*;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub use self::point::*;
+pub use self::stroke::*;
+pub use self::classifier::*;
+pub use self::stroke_sample::*;
